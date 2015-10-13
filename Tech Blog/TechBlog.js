ae0ba048f5d05e60e0816ad4fb65a72de1f3ceaf
Evgeniy <golddgek3@gmail.com>
2015-10-13 19:56:31 +0300

    Tech Blog

---
+++ Tech Blog/TechBlog.js
@@ -0,0 +1,9 @@
+    function showElement(layer){
+    var myLayer = document.getElementById(layer);
+    if(myLayer.style.display=="none"){
+     myLayer.style.display="block";
+     myLayer.backgroundPosition="top";
+     } else { 
+     myLayer.style.display="none";
+     }
+    }
